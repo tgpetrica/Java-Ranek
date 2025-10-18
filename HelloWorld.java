1f596f66c9c3994157db73775d45db56136f3908
Teodor <teodor.petrica@rovacc.ro>
2025-10-18 10:29:26 +0300

    add L2

---
+++ HelloWorld.java
@@ -0,0 +1,15 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
+
+/* in C++
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+    return 0;
+}
+ */
