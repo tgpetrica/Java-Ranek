0136c2e4433ab38b958c245e98161110e34e6f1a
Teodor <teodor.petrica@rovacc.ro>
2026-04-25 12:15:24 +0300

    add L27

--- CitireFisier.java
+++ CitireFisier.java
@@ -2,15 +2,8 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class CitireFisier {
-    public static void main(String[] args) {
-        try {
-            System.out.println("mesaj");
-            FileReader fr = new FileReader("input.txt");
-            System.out.println("tr");
-        } catch (IOException e) {
-            System.out.println("fisierul nu a fost gasit.");
-        } finally {
-            System.out.println("stop");
-        }
+    public static void main(String[] args) throws IOException{
+        FileReader fr = new FileReader("input.txt");
+        
     }
 }
