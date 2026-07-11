b3b8253fecd6d99a4f5366d51ad19f62f196cfd8
Teodor <teodor.petrica@rovacc.ro>
2026-07-11 11:25:59 +0300

    add accel ja65

---
+++ Comp.java
@@ -0,0 +1,36 @@
+public class Comp {
+    int atribut;
+    public static void main(String[] args) {
+        int a = 10;
+        System.out.println(a);
+        //int varsta = "23";
+        //int rezultat = 10/0;
+
+        //System.out.println(nume);
+        String nume = null;
+        System.out.println(nume.length());
+        
+        int score = 100;
+        /*
+        TOKENs
+            int     -   keyword
+            score   -   identificator
+            =       -   operator
+            100     -   numar
+            ;       -   separator
+         */
+
+        String oras = "Amsterdam";
+        /*
+        TOKENs
+            String      -   keyword
+            oras        -   identificator
+            =           -   operator
+            "Amsterdam" -   valoare (String)
+            ;           -   separator
+        */
+
+        int x = 20;
+        // int = y 25;
+    }
+}
